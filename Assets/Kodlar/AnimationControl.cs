5c14a12deb56a9430e8b419e0b199750da1e7e0c
azizkale <azizkale@hotmail.com>
2021-11-21 20:25:08 +0100

    DoesntWorkWell

--- Assets/Kodlar/AnimationControl.cs
+++ Assets/Kodlar/AnimationControl.cs
@@ -5,26 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class AnimationControl : MonoBehaviour
 {
-    public Animation anim;
     void Start()
     {
-        anim = GetComponent<Animation>();
-
+        //PlayerPrefs.DeleteAll();
+        if (PlayerPrefs.GetInt("animationReshowing") == 0)
+        {
+            PlayerPrefs.SetInt("animationReshowing", 1);
+            //SceneManager.LoadScene("IntroAnimation");
+        }
+        if (PlayerPrefs.GetInt("animationReshowing") == 1)
+        {
+            SceneManager.LoadScene("no100_11");
+           
+        }
+        //else
+            //SceneManager.LoadScene("IntroAnimation");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void watchAgain()
     {
-        anim.Play("Intro");
+        PlayerPrefs.SetInt("animationReshowing", 0);
+        SceneManager.LoadScene("IntroAnimation");
     }
 
     public void dontShowAgain()
     {
+        PlayerPrefs.SetInt("animationReshowing", 1);
         SceneManager.LoadScene("no100_11");
     }
 }
