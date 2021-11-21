4439453cf84d9b99dd7c841c112daa5d9a36970e
azizkale <azizkale@hotmail.com>
2021-11-21 17:13:51 +0100

    AnimationControl Script added

---
+++ Assets/Kodlar/AnimationControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationControl : MonoBehaviour
+{
+    public Animation anim;
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void watchAgain()
+    {
+        anim.Play("Intro");
+    }
+
+    public void dontShowAgain()
+    {
+        SceneManager.LoadScene("no100_11");
+    }
+}
