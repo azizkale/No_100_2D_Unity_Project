43e2a215e5d561cdfbd8c6d61010d5761f6283ec
azizkale <azizkale@hotmail.com>
2021-11-24 13:40:27 +0100

    WorksWell

--- Assets/Kodlar/AnimationControl.cs
+++ Assets/Kodlar/AnimationControl.cs
@@ -2,8 +2,16 @@
 
 public class AnimationControl : MonoBehaviour
 {
+    public Animator animIntro;
     void Start()
     {
+        //PlayerPrefs.SetInt("animIntro", 0);
+
+        if(PlayerPrefs.GetInt("animIntro") == 0)
+        {
+            animIntro.SetTrigger("fire");
+            PlayerPrefs.SetInt("animIntro", 1);
+        }
     }
 
     private void Update()
