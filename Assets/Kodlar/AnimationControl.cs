97a8d11163e475c84013d217107378820c5ea396
azizkale <azizkale@hotmail.com>
2021-11-24 11:13:27 +0100

    commit

--- Assets/Kodlar/AnimationControl.cs
+++ Assets/Kodlar/AnimationControl.cs
@@ -2,10 +2,8 @@
 
 public class AnimationControl : MonoBehaviour
 {
-    public GameObject go;
     void Start()
     {
-        PlayerPrefs.SetInt("animation", 0);
     }
 
     private void Update()
@@ -25,18 +23,6 @@ public class AnimationControl : MonoBehaviour
     public void showAnimationIntro()
     {
 
-        if (PlayerPrefs.GetInt("animation") == 0)
-        {
-            PlayerPrefs.SetInt("animation", 1);
-            go.SetActive(true);
-            Debug.Log(PlayerPrefs.GetInt("animation"));
-        }
-        if(PlayerPrefs.GetInt("animation") == 1)
-        {
-            PlayerPrefs.SetInt("animation", 0);
-            go.SetActive(false);
-            Debug.Log(PlayerPrefs.GetInt("animation"));
-        }
-        //return PlayerPrefs.GetInt("animation");
+       
     }
 }
