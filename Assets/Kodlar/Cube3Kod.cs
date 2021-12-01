46b70913dec3ce8e985b67e4dce2da8caffaf4c1
azizkale <azizkale@hotmail.com>
2021-12-01 09:20:08 +0100

    CubeFalling Animation is canclled, Cube spinning anim works well. memory system is done better

--- Assets/Kodlar/Cube3Kod.cs
+++ Assets/Kodlar/Cube3Kod.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Cube3Kod : MonoBehaviour
@@ -10,19 +11,19 @@ public class Cube3Kod : MonoBehaviour
     bool availableMove = false; // if there are availables move(blue cube) it turns to true
     ScorControl scorControl;
     AnimationControl animcontrol;
-
+    Memory memo;
     void Start()
     {
         render = GetComponent<Renderer>();
         oyunKontrol = GameObject.FindGameObjectWithTag("oyunKontrolTag").GetComponent<OyunKontrolKod>();
         scorControl = new ScorControl();
         animcontrol = new AnimationControl();
+        memo = new Memory();
     }
 
     void OnMouseDown()
     {
-        KutularaTiklama();
-        StartCoroutine(DonmeAnimasyonu());
+        KutularaTiklama();       
         Debug.Log(name);
     } 
     
@@ -31,12 +32,15 @@ public class Cube3Kod : MonoBehaviour
     {
         int index = 0;
         int syc = System.Int32.Parse(this.name);// clone küpün adını int e çevirir
+        
+        //spinning animation
+        StartCoroutine(DonmeAnimasyonu(oyunKontrol.clonelar[syc]));
 
         // altattaki foreach döngüsü yesil olan clone küpe sayı veriri
         foreach (GameObject item in oyunKontrol.clonelar)
         {
             if (item.tag=="yesil")
-            {               
+            {   
                 index++;
                 animcontrol.scoarBoardSwingig(index);
             }
@@ -47,9 +51,8 @@ public class Cube3Kod : MonoBehaviour
         {            
             if (oyunKontrol.clonelar[i].tag!="yesil") // yeşil olan küp rengi değişmesin diye
             {
-                render.material.mainTexture = oyunKontrol.sayilar[index];
-
                 tag = "yesil";
+                render.material.mainTexture = oyunKontrol.sayilar[index];                
 
                 oyunKontrol.clonelar[syc].layer = 2; // yeşil küpe tıklanmasın diye                
 
@@ -69,7 +72,10 @@ public class Cube3Kod : MonoBehaviour
                     oyunKontrol.clonelar[i].layer = 2;
                 }                
             }            
-        }    
+        }
+
+        // code below saves texture number of every cubes in evey click to be able to load from memoy
+        memo.saveTextureNumber("texture:"+this.name, index);
 
         index++; // yesil olan küpe sayı verme index i
 
@@ -77,12 +83,12 @@ public class Cube3Kod : MonoBehaviour
         scorControl.setAndSaveScores(index);
     }
 
-    public IEnumerator DonmeAnimasyonu()
+    public IEnumerator DonmeAnimasyonu(GameObject go) // arbitary variable
     {
         int index = 1;
         for (int i = 0; i <= index; i++)
         {           
-            this.transform.rotation = Quaternion.Euler(-i * 5, 0, -180);
+            go.transform.rotation = Quaternion.Euler(-i * 5, 0, -180);
             yield return new WaitForSeconds(0.1f);
             index++;
         }
